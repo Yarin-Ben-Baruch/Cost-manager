9d7a971803b94c33f2ab74f05ab29af5fe3e2160
yarinbenbaruch <yarinbenbaruch97@gmail.com>
2021-12-03 10:38:38 +0200

    Update item class

--- src/Item.java
+++ src/Item.java
@@ -1,12 +1,13 @@
-import java.util.ArrayList;
+import javax.xml.crypto.Data;
 
 public class Item {
 
+    private int id;
+    private String name;
     private String currency;
     private double sum;
     private String describing;
     private Category category;
-
-
+    private Data data;
 
 }
