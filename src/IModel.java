e6b213982ecaef34788716c5735420a9b59eff0d
matankorenfeld <m4t4n04@gmail.com>
2021-12-03 10:03:28 +0200

    add report class update name of methods

--- src/IModel.java
+++ src/IModel.java
@@ -2,6 +2,6 @@ import java.util.Collection;
 
 public interface IModel {
     public void addItem(Item item) throws CostMangerException;
-    public Collection<Item> getItems() throws CostMangerException;
+    public Report getDetailedReport() throws CostMangerException;
     public void addNewCategory(Category category) throws CostMangerException;
 }
