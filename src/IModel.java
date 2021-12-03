e64a559222231d96f182392ab7002570110df031
matankorenfeld <m4t4n04@gmail.com>
2021-12-03 10:20:56 +0200

    update Imodel remove report

--- src/IModel.java
+++ src/IModel.java
@@ -2,6 +2,6 @@ import java.util.Collection;
 
 public interface IModel {
     public void addItem(Item item) throws CostMangerException;
-    public Report getDetailedReport() throws CostMangerException;
+    public Collection<Item> getDetailedReport(Item[] item,String date) throws CostMangerException;
     public void addNewCategory(Category category) throws CostMangerException;
 }
--- src/Item.java
+++ src/Item.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+
 public class Item {
 
     private String currency;
     private double sum;
     private String describing;
     private Category category;
+
+
 
 }
