272866b49add4dd32615488ae69f04bddace15e2
matankorenfeld <m4t4n04@gmail.com>
2021-12-13 20:18:28 +0200

    fix bug in CostMangerGUIView

--- src/il/ac/hit/IView.java
+++ src/il/ac/hit/IView.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 
 public interface IView {
 
+    void showItems(Collection<Item> i_Items);
+
     void setIViewModel(IViewModel i_Vm);
     void showMessage(Message i_Message);
     void init();
