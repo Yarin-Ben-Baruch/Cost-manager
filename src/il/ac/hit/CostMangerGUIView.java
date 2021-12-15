1e1720bdf81ed989b044d86910561dd14546d382
matankorenfeld <m4t4n04@gmail.com>
2021-12-15 13:06:03 +0200

    update GUIVIEW

--- src/il/ac/hit/CostMangerGUIView.java
+++ src/il/ac/hit/CostMangerGUIView.java
@@ -8,6 +8,10 @@ import java.util.Collection;
 
 public class CostMangerGUIView implements IView {
 
+    private String[] days;
+    private String[] months;
+    private IViewModel vm;
+
     private JFrame m_MainFrame;
     private JPanel m_CostPanel;
     private JPanel m_ButtonsPanel;
@@ -45,7 +49,6 @@ public class CostMangerGUIView implements IView {
     private JLabel m_AddCategoryLabel;
     private JButton m_AddCategoryToDBButton;
 
-
     // Add Item Button, TextField, Label and Submit Action.
     private JPanel m_AddItemPanel;
     private JButton m_AddItemButton;
@@ -57,15 +60,15 @@ public class CostMangerGUIView implements IView {
     private JLabel m_AddItemCategoryLabel, m_AddItemSumLabel,m_AddItemDayLabel, m_AddItemMonthLabel;
 
 
-    private IViewModel vm;
-
     @Override
     public void init() {
-        String[] days = new String[31];
+
+        // remove to method !
+        days = new String[31];
         for (int i =0 ; i<31 ; i++) {
             days[i] = (i+1) +"";
         }
-        String[] months =new String[12];
+        months = new String[12];
         for (int i =0 ; i<12 ; i++) {
             months[i] = (i+1) +"";
         }
@@ -73,7 +76,7 @@ public class CostMangerGUIView implements IView {
         // Create JFrame
         m_MainFrame = new JFrame();
 
-        // Create all the JPanels.
+        // Create JPanels.
         m_ButtonsPanel = new JPanel();
         m_MessagePanel = new JPanel();
         m_CostPanel = new JPanel();
@@ -82,66 +85,11 @@ public class CostMangerGUIView implements IView {
         m_ShowItemsButton = new JButton("Show Items");
         m_ShowCategoriesButton = new JButton("Show Categories");
 
-        // Creating the detailed report Action.
-        m_ShowReportPanel = new JPanel();
-        m_ShowReportButton = new JButton("Show detail report");
-        m_ShowReportStartDateDayLabel = new JLabel("Start day:");
-        m_ShowReportStartDateDayTextField = new JComboBox(days);
-        m_ShowReportStartDateMonthLabel = new JLabel("Start month:");
-        m_ShowReportStartDateMonthTextField = new JComboBox(months);
-        m_ShowReportEndDateDayLabel = new JLabel("End day:");
-        m_ShowReportEndDateDayTextField = new JComboBox(days);
-        m_ShowReportEndDateMonthLabel = new JLabel("End Month");
-        m_ShowReportEndDateMonthTextField = new JComboBox(months);
-        m_ShowReportActionButton = new JButton("Show the report");
-
-        // Creating the AddItem Action.
-        m_AddItemPanel = new JPanel();
-        m_AddItemButton = new JButton("Add item");
-        m_AddItemNameLabel = new JLabel("Name:");
-        m_AddItemNameTextField = new JTextField(10);
-        m_AddItemDescribingLabel = new JLabel("Descrption:");
-        m_AddItemDescribingTextField = new JTextField(30);
-        m_AddItemCurrencyLabel = new JLabel("Currency:");
-        m_AddItemCurrencyTextField = new JTextField(8);
-        m_AddItemCategoryLabel = new JLabel("Category:");
-        m_AddItemCategoryTextField = new JTextField(10);
-        m_AddItemSumLabel = new JLabel("Sum");
-        m_AddItemSumTextField = new JTextField(8);
-        m_AddItemDayLabel = new JLabel("Day:");
-        m_AddItemDayComboBox = new JComboBox(days);
-        m_AddItemMonthLabel = new JLabel("Month");
-        m_AddItemMonthComboBox = new JComboBox(months);
-        m_AddItemToDBButton = new JButton("Add cost to the list");
-
-        // Creating the AddCategory Action.
-        m_AddCategoryPanel = new JPanel();
-        m_AddCategoryLabel = new JLabel("Category name:");
-        m_AddCategory = new JButton("Add category");
-        m_AddCategoryTextField = new JTextField(40);
-        m_AddCategoryToDBButton = new JButton("Add category to the list");
-
-        // Creating the RemoveItem Action
-        m_RemoveItemPanel = new JPanel();
-        m_RemoveItemButton = new JButton("Remove item");
-        m_RemoveItemFromDBButton = new JButton("Remove Item from the list");
-        m_RemoveItemCostNumberLabel = new JLabel("Cost number:");
-        m_RemoveItemCostNumberTextField = new JTextField(10);
-        m_RemoveItemUsernameLabel = new JLabel("Username:");
-        m_RemoveItemUsernameTextField = new JTextField(15);
-
-        // Creating the UpdateItem Action
-        m_UpdateItemPanel = new JPanel();
-        m_UpdateItemButton = new JButton("Update item");
-        m_UpdateItemColNameLabel = new JLabel("Col name to change:");
-        m_UpdateItemColNameTextField = new JTextField(15);
-        m_UpdateItemDataToSetLabel = new JLabel("Date to set:");
-        m_UpdateItemDataToSetTextField = new JTextField(15);
-        m_UpdateItemToDBButton = new JButton("Update item to DB");
-        m_UpdateItemCostNumberLabel = new JLabel("Cost number:");
-        m_UpdateItemCostNumberTextField = new JTextField(10);
-        m_UpdateItemUsernameLabel = new JLabel("Username:");
-        m_UpdateItemUsernameTextField = new JTextField(15);
+        showReportInit();
+        addItemInit();
+        addCategoryInit();
+        removeItemInit();
+        updateItemInit();
 
         // Create Message TextField.
         m_MessageTextField = new JTextField(40);
@@ -159,91 +107,13 @@ public class CostMangerGUIView implements IView {
         // Add the message panel to the main frame
         m_MainFrame.add(m_MessagePanel,BorderLayout.NORTH);
 
-        // Creating the Button Panel.
-        m_ButtonsPanel.setLayout(new FlowLayout());
-        m_ButtonsPanel.add(m_ShowItemsButton);
-        m_ButtonsPanel.add(m_ShowCategoriesButton);
-        m_ButtonsPanel.add(m_ShowReportButton);
-        m_ButtonsPanel.add(m_AddItemButton);
-        m_ButtonsPanel.add(m_AddCategory);
-        m_ButtonsPanel.add(m_RemoveItemButton);
-        m_ButtonsPanel.add(m_UpdateItemButton);
-        m_MainFrame.add(m_ButtonsPanel,BorderLayout.CENTER);
-
-        // Creating the detailed report Panel.
-        m_ShowReportPanel.setLayout(new GridLayout(5,2));
-        m_ShowReportPanel.add(m_ShowReportStartDateDayLabel);
-        m_ShowReportPanel.add(m_ShowReportStartDateDayTextField);
-        m_ShowReportPanel.add(m_ShowReportStartDateMonthLabel);
-        m_ShowReportPanel.add(m_ShowReportStartDateMonthTextField);
-        m_ShowReportPanel.add(m_ShowReportEndDateDayLabel);
-        m_ShowReportPanel.add(m_ShowReportEndDateDayTextField);
-        m_ShowReportPanel.add(m_ShowReportEndDateMonthLabel);
-        m_ShowReportPanel.add(m_ShowReportEndDateMonthTextField);
-        m_ShowReportPanel.add(m_ShowReportActionButton);
-        m_ShowReportPanel.setVisible(false);
-        m_MainFrame.add(m_ShowReportPanel,BorderLayout.CENTER);
-
-        // Creating the Update Item Panel.
-        m_UpdateItemPanel.setLayout(new GridLayout(5,2));
-        m_UpdateItemPanel.add(m_UpdateItemColNameLabel);
-        m_UpdateItemPanel.add(m_UpdateItemColNameTextField);
-        m_UpdateItemPanel.add(m_UpdateItemDataToSetLabel);
-        m_UpdateItemPanel.add(m_UpdateItemDataToSetTextField);
-        m_UpdateItemPanel.add(m_UpdateItemCostNumberLabel);
-        m_UpdateItemPanel.add(m_UpdateItemCostNumberTextField);
-        m_UpdateItemPanel.add(m_UpdateItemUsernameLabel);
-        m_UpdateItemPanel.add(m_UpdateItemUsernameTextField);
-        m_UpdateItemPanel.add(m_UpdateItemCostNumberLabel);
-        m_UpdateItemPanel.add(m_UpdateItemCostNumberTextField);
-        m_UpdateItemPanel.add(m_UpdateItemUsernameLabel);
-        m_UpdateItemPanel.add(m_UpdateItemUsernameTextField);
-        m_UpdateItemPanel.add(m_UpdateItemToDBButton);
-        m_UpdateItemPanel.setVisible(false);
-        m_MainFrame.add(m_UpdateItemPanel,BorderLayout.CENTER);
-
-        // Creating the Remove Item Panel.
-        m_RemoveItemPanel.setLayout(new GridLayout(4,2));
-        m_RemoveItemPanel.add(m_RemoveItemCostNumberLabel);
-        m_RemoveItemPanel.add(m_RemoveItemCostNumberTextField);
-        m_RemoveItemPanel.add(m_RemoveItemUsernameLabel);
-        m_RemoveItemPanel.add(m_RemoveItemUsernameTextField);
-        m_RemoveItemPanel.add(m_RemoveItemFromDBButton);
-        m_RemoveItemPanel.setVisible(false);
-        m_MainFrame.add(m_RemoveItemPanel,BorderLayout.CENTER);
-
-        // Creating the Add Category Panel.
-        m_AddCategoryPanel.setLayout(new GridLayout(2,2));
-        m_AddCategoryPanel.add(m_AddCategoryLabel);
-        m_AddCategoryPanel.add(m_AddCategoryTextField);
-        m_AddCategoryPanel.add(m_AddCategoryToDBButton);
-        m_AddCategoryPanel.setVisible(false);
-        m_MainFrame.add(m_AddCategoryPanel,BorderLayout.CENTER);
-
-        // Creating the Add Item Panel.
-        m_AddItemPanel.setLayout(new GridLayout(5,2));
-        m_AddItemPanel.add(m_AddItemNameLabel);
-        m_AddItemPanel.add(m_AddItemNameTextField);
-        m_AddItemPanel.add(m_AddItemCurrencyLabel);
-        m_AddItemPanel.add(m_AddItemCurrencyTextField);
-        m_AddItemPanel.add(m_AddItemCategoryLabel);
-        m_AddItemPanel.add(m_AddItemCategoryTextField);
-        m_AddItemPanel.add(m_AddItemSumLabel);
-        m_AddItemPanel.add(m_AddItemSumTextField);
-        m_AddItemPanel.add(m_AddItemDayLabel);
-        m_AddItemPanel.add(m_AddItemDayComboBox);
-        m_AddItemPanel.add(m_AddItemMonthLabel);
-        m_AddItemPanel.add(m_AddItemMonthComboBox);
-        m_AddItemPanel.add(m_AddItemDescribingLabel);
-        m_AddItemPanel.add(m_AddItemDescribingTextField);
-        m_AddItemPanel.add(m_AddItemToDBButton);
-        m_AddItemPanel.setVisible(false);
-        m_MainFrame.add(m_AddItemPanel,BorderLayout.CENTER);
-
-        // Creating the cost Area.
-        m_CostPanel.setLayout(new FlowLayout());
-        m_CostPanel.add(m_CostInfoTextArea);
-        m_MainFrame.add(m_CostPanel,BorderLayout.SOUTH);
+        creatingButtonsStart();
+        showReportStart();
+        updateItemStart();
+        removeItemStart();
+        addCategoryStart();
+        addItemStart();
+        creatingCostAreaStart();
 
         // Setting the MainFrame Layout
         m_MainFrame.setLayout(new FlowLayout());
@@ -365,6 +235,179 @@ public class CostMangerGUIView implements IView {
         });
     }
 
+    private void removeItemInit() {
+        // Creating the RemoveItem Action
+        m_RemoveItemPanel = new JPanel();
+        m_RemoveItemButton = new JButton("Remove item");
+        m_RemoveItemFromDBButton = new JButton("Remove Item from the list");
+        m_RemoveItemCostNumberLabel = new JLabel("Cost number:");
+        m_RemoveItemCostNumberTextField = new JTextField(10);
+        m_RemoveItemUsernameLabel = new JLabel("Username:");
+        m_RemoveItemUsernameTextField = new JTextField(15);
+
+    }
+
+    private void addCategoryInit() {
+        // Creating the AddCategory Action.
+        m_AddCategoryPanel = new JPanel();
+        m_AddCategoryLabel = new JLabel("Category name:");
+        m_AddCategory = new JButton("Add category");
+        m_AddCategoryTextField = new JTextField(40);
+        m_AddCategoryToDBButton = new JButton("Add category to the list");
+    }
+
+    private void updateItemInit() {
+        // Creating the UpdateItem Action
+        m_UpdateItemPanel = new JPanel();
+        m_UpdateItemButton = new JButton("Update item");
+        m_UpdateItemColNameLabel = new JLabel("Col name to change:");
+        m_UpdateItemColNameTextField = new JTextField(15);
+        m_UpdateItemDataToSetLabel = new JLabel("Date to set:");
+        m_UpdateItemDataToSetTextField = new JTextField(15);
+        m_UpdateItemToDBButton = new JButton("Update item to DB");
+        m_UpdateItemCostNumberLabel = new JLabel("Cost number:");
+        m_UpdateItemCostNumberTextField = new JTextField(10);
+        m_UpdateItemUsernameLabel = new JLabel("Username:");
+        m_UpdateItemUsernameTextField = new JTextField(15);
+    }
+
+    private void addItemInit() {
+        // Creating the AddItem Action.
+        m_AddItemPanel = new JPanel();
+        m_AddItemButton = new JButton("Add item");
+        m_AddItemNameLabel = new JLabel("Name:");
+        m_AddItemNameTextField = new JTextField(10);
+        m_AddItemDescribingLabel = new JLabel("Descrption:");
+        m_AddItemDescribingTextField = new JTextField(30);
+        m_AddItemCurrencyLabel = new JLabel("Currency:");
+        m_AddItemCurrencyTextField = new JTextField(8);
+        m_AddItemCategoryLabel = new JLabel("Category:");
+        m_AddItemCategoryTextField = new JTextField(10);
+        m_AddItemSumLabel = new JLabel("Sum");
+        m_AddItemSumTextField = new JTextField(8);
+        m_AddItemDayLabel = new JLabel("Day:");
+        m_AddItemDayComboBox = new JComboBox(days);
+        m_AddItemMonthLabel = new JLabel("Month");
+        m_AddItemMonthComboBox = new JComboBox(months);
+        m_AddItemToDBButton = new JButton("Add cost to the list");
+    }
+
+    private void showReportInit() {
+        // Creating the detailed report Action.
+        m_ShowReportPanel = new JPanel();
+        m_ShowReportButton = new JButton("Show detail report");
+        m_ShowReportStartDateDayLabel = new JLabel("Start day:");
+        m_ShowReportStartDateDayTextField = new JComboBox(days);
+        m_ShowReportStartDateMonthLabel = new JLabel("Start month:");
+        m_ShowReportStartDateMonthTextField = new JComboBox(months);
+        m_ShowReportEndDateDayLabel = new JLabel("End day:");
+        m_ShowReportEndDateDayTextField = new JComboBox(days);
+        m_ShowReportEndDateMonthLabel = new JLabel("End Month");
+        m_ShowReportEndDateMonthTextField = new JComboBox(months);
+        m_ShowReportActionButton = new JButton("Show the report");
+    }
+
+    private void showReportStart() {
+        // Creating the detailed report Panel.
+        m_ShowReportPanel.setLayout(new GridLayout(5,2));
+        m_ShowReportPanel.add(m_ShowReportStartDateDayLabel);
+        m_ShowReportPanel.add(m_ShowReportStartDateDayTextField);
+        m_ShowReportPanel.add(m_ShowReportStartDateMonthLabel);
+        m_ShowReportPanel.add(m_ShowReportStartDateMonthTextField);
+        m_ShowReportPanel.add(m_ShowReportEndDateDayLabel);
+        m_ShowReportPanel.add(m_ShowReportEndDateDayTextField);
+        m_ShowReportPanel.add(m_ShowReportEndDateMonthLabel);
+        m_ShowReportPanel.add(m_ShowReportEndDateMonthTextField);
+        m_ShowReportPanel.add(m_ShowReportActionButton);
+        m_ShowReportPanel.setVisible(false);
+        m_MainFrame.add(m_ShowReportPanel,BorderLayout.CENTER);
+    }
+
+    private void updateItemStart() {
+        // Creating the Update Item Panel.
+        m_UpdateItemPanel.setLayout(new GridLayout(5,2));
+        m_UpdateItemPanel.add(m_UpdateItemColNameLabel);
+        m_UpdateItemPanel.add(m_UpdateItemColNameTextField);
+        m_UpdateItemPanel.add(m_UpdateItemDataToSetLabel);
+        m_UpdateItemPanel.add(m_UpdateItemDataToSetTextField);
+        m_UpdateItemPanel.add(m_UpdateItemCostNumberLabel);
+        m_UpdateItemPanel.add(m_UpdateItemCostNumberTextField);
+        m_UpdateItemPanel.add(m_UpdateItemUsernameLabel);
+        m_UpdateItemPanel.add(m_UpdateItemUsernameTextField);
+        m_UpdateItemPanel.add(m_UpdateItemCostNumberLabel);
+        m_UpdateItemPanel.add(m_UpdateItemCostNumberTextField);
+        m_UpdateItemPanel.add(m_UpdateItemUsernameLabel);
+        m_UpdateItemPanel.add(m_UpdateItemUsernameTextField);
+        m_UpdateItemPanel.add(m_UpdateItemToDBButton);
+        m_UpdateItemPanel.setVisible(false);
+        m_MainFrame.add(m_UpdateItemPanel,BorderLayout.CENTER);
+    }
+
+    private void removeItemStart() {
+        // Creating the Remove Item Panel.
+        m_RemoveItemPanel.setLayout(new GridLayout(4,2));
+        m_RemoveItemPanel.add(m_RemoveItemCostNumberLabel);
+        m_RemoveItemPanel.add(m_RemoveItemCostNumberTextField);
+        m_RemoveItemPanel.add(m_RemoveItemUsernameLabel);
+        m_RemoveItemPanel.add(m_RemoveItemUsernameTextField);
+        m_RemoveItemPanel.add(m_RemoveItemFromDBButton);
+        m_RemoveItemPanel.setVisible(false);
+        m_MainFrame.add(m_RemoveItemPanel,BorderLayout.CENTER);
+    }
+
+    private void addCategoryStart() {
+        // Creating the Add Category Panel.
+        m_AddCategoryPanel.setLayout(new GridLayout(2,2));
+        m_AddCategoryPanel.add(m_AddCategoryLabel);
+        m_AddCategoryPanel.add(m_AddCategoryTextField);
+        m_AddCategoryPanel.add(m_AddCategoryToDBButton);
+        m_AddCategoryPanel.setVisible(false);
+        m_MainFrame.add(m_AddCategoryPanel,BorderLayout.CENTER);
+    }
+
+    private void addItemStart() {
+        // Creating the Add Item Panel.
+        m_AddItemPanel.setLayout(new GridLayout(5,2));
+        m_AddItemPanel.add(m_AddItemNameLabel);
+        m_AddItemPanel.add(m_AddItemNameTextField);
+        m_AddItemPanel.add(m_AddItemCurrencyLabel);
+        m_AddItemPanel.add(m_AddItemCurrencyTextField);
+        m_AddItemPanel.add(m_AddItemCategoryLabel);
+        m_AddItemPanel.add(m_AddItemCategoryTextField);
+        m_AddItemPanel.add(m_AddItemSumLabel);
+        m_AddItemPanel.add(m_AddItemSumTextField);
+        m_AddItemPanel.add(m_AddItemDayLabel);
+        m_AddItemPanel.add(m_AddItemDayComboBox);
+        m_AddItemPanel.add(m_AddItemMonthLabel);
+        m_AddItemPanel.add(m_AddItemMonthComboBox);
+        m_AddItemPanel.add(m_AddItemDescribingLabel);
+        m_AddItemPanel.add(m_AddItemDescribingTextField);
+        m_AddItemPanel.add(m_AddItemToDBButton);
+        m_AddItemPanel.setVisible(false);
+        m_MainFrame.add(m_AddItemPanel,BorderLayout.CENTER);
+    }
+
+    private void creatingButtonsStart() {
+        // Creating the Button Panel.
+        m_ButtonsPanel.setLayout(new FlowLayout());
+        m_ButtonsPanel.add(m_ShowItemsButton);
+        m_ButtonsPanel.add(m_ShowCategoriesButton);
+        m_ButtonsPanel.add(m_ShowReportButton);
+        m_ButtonsPanel.add(m_AddItemButton);
+        m_ButtonsPanel.add(m_AddCategory);
+        m_ButtonsPanel.add(m_RemoveItemButton);
+        m_ButtonsPanel.add(m_UpdateItemButton);
+        m_MainFrame.add(m_ButtonsPanel,BorderLayout.CENTER);
+    }
+
+    private void creatingCostAreaStart() {
+        // Creating the cost Area.
+        m_CostPanel.setLayout(new FlowLayout());
+        m_CostPanel.add(m_CostInfoTextArea);
+        m_MainFrame.add(m_CostPanel,BorderLayout.SOUTH);
+    }
+
+
     /**
      * A method that gets collection of items and print the items into the TextArea.
      * @param i_Items
@@ -419,5 +462,7 @@ public class CostMangerGUIView implements IView {
     public void showMessage(Message i_Message) {
         m_MessageTextField.setText(i_Message.getText());
     }
+
+
 
 }
