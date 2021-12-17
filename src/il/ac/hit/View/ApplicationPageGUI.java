7b47ca626732da4c85ae10874909e91714ad4048
matankorenfeld <m4t4n04@gmail.com>
2021-12-17 15:07:51 +0200

    Update ApplicationPageGUI

--- src/il/ac/hit/View/ApplicationPageGUI.java
+++ src/il/ac/hit/View/ApplicationPageGUI.java
@@ -3,10 +3,10 @@ package il.ac.hit.View;
 import il.ac.hit.*;
 import il.ac.hit.Model.Category;
 import il.ac.hit.Model.Item;
-import il.ac.hit.Model.User;
 import il.ac.hit.ViewModel.IViewModel;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,6 +15,8 @@ import java.util.LinkedList;
 
 public class ApplicationPageGUI {
     private IViewModel m_ViewModel;
+    private boolean isCostsAppears;
+    private boolean isCategoriesAppears;
 
     private JFrame m_MainFrame;
     private JPanel m_ButtonsPanel;
@@ -36,6 +38,8 @@ public class ApplicationPageGUI {
 
     public ApplicationPageGUI(IViewModel i_Vm) {
         m_ViewModel = i_Vm;
+        isCostsAppears = false;
+        isCategoriesAppears = false;
     }
 
     public void initApplication() {
@@ -73,6 +77,7 @@ public class ApplicationPageGUI {
 //        menu.add("Logout");
 //        menuBar.add(menu);
 //        m_MainFrame.setMenuBar(menuBar);
+
 
         // Setting the MainFrame Layout
         m_MainFrame.setLayout(new FlowLayout());
@@ -149,15 +154,21 @@ public class ApplicationPageGUI {
 
     /**
      * A method that gets collection of items and print the items into the Table.
+     *
      * @param i_Items
      */
     public void showItems(Collection<Item> i_Items) {
 
+//        isCostsAppears = !isCostsAppears;
+//        DefaultTableModel model = (DefaultTableModel) m_ItemsTable.getModel();
+//        model.removeRow();
+
         LinkedList<Item> items = (LinkedList<Item>) i_Items;
         String[][] data = new String[i_Items.size()][7];
-        String[] columnNames = { "Cost number", "Name", "Description", "Currency", "Category", "Sum", "Date"};
+        String[] columnNames = {"Cost number", "Name", "Description", "Currency", "Category", "Sum", "Date"};
 
-        for(int i = 0 ; i < i_Items.size() ; i++){
+        for (int i = 0; i < i_Items.size(); i++) {
+//                model.addRow(data);
             data[i][0] = String.valueOf(items.get(i).getCostNumber());
             data[i][1] = items.get(i).getName();
             data[i][2] = items.get(i).getDescribing();
@@ -173,22 +184,25 @@ public class ApplicationPageGUI {
 
         scrollPanel = new JScrollPane(m_ItemsTable);
 
-        m_MainFrame.add(scrollPanel,BorderLayout.CENTER);
+        m_MainFrame.add(scrollPanel, BorderLayout.CENTER);
         m_MainFrame.setVisible(true);
+
     }
 
     /**
      * A method that gets collection of categories and print the items into the TextArea.
+     *
      * @param i_Categories
      */
     public void ShowCategories(Collection<Category> i_Categories) {
+
 
         LinkedList<Category> categories = (LinkedList<Category>) i_Categories;
 
         String[][] data = new String[i_Categories.size()][1];
         String[] columnNames = {"Category"};
 
-        for(int i = 0 ; i < i_Categories.size() ; i++){
+        for (int i = 0; i < i_Categories.size(); i++) {
             data[i][0] = String.valueOf(categories.get(i).getCategoryName());
         }
 
@@ -198,8 +212,10 @@ public class ApplicationPageGUI {
 
         scrollPanel = new JScrollPane(m_ItemsTable);
 
-        m_MainFrame.add(scrollPanel,BorderLayout.CENTER);
+        m_MainFrame.add(scrollPanel, BorderLayout.CENTER);
         m_MainFrame.setVisible(true);
+
+
     }
 
     /**
